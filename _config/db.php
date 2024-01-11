56ecb8f35ab0f7c9d5371511fd2ef507635b333e
Mohammed Elghanam <144786058+MohammedElghanam@users.noreply.github.com>
2024-01-11 20:22:02 +0100

    handel the error in ajax

--- _config/db.php
+++ _config/db.php
@@ -1,3 +1,14 @@
 <?php
 
-$db = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+//  $db = mysqli_connect($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
+
+try {
+    $db = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME",$DB_USER,$DB_PASS);
+
+    echo "connected";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+
+
--- models/dashboard_model.php
+++ models/dashboard_model.php
@@ -1,40 +1,40 @@
 <?php
-class DASHBOARD{
-    public $db;
+// class DASHBOARD{
+//     public $db;
 
-    public function select(){
-        global $db;
-        $user_id = $_SESSION['user_id'];
-        $sql = " SELECT * FROM signup ";
-        $result = mysqli_query($db,$sql);
+//     public function select(){
+//         global $db;
+//         $user_id = $_SESSION['user_id'];
+//         $sql = " SELECT * FROM signup ";
+//         $result = mysqli_query($db,$sql);
 
-        if (!$result) {
-            die("error in selecting");
-        }else {
-            return $result;
-        }
+//         if (!$result) {
+//             die("error in selecting");
+//         }else {
+//             return $result;
+//         }
 
-    }
+//     }
 
     
 
-    public function delet($user_id){
-        global $db;
+//     public function delet($user_id){
+//         global $db;
         
         
-        $sql =  "DELETE FROM signup WHERE user_id = $user_id ";
-        $result = mysqli_query($db,$sql);
-        if (!$result) {
-            die("error in deleting");
-        }else {
-            header('location:index.php?page=dashboard');
-        }
+//         $sql =  "DELETE FROM signup WHERE user_id = $user_id ";
+//         $result = mysqli_query($db,$sql);
+//         if (!$result) {
+//             die("error in deleting");
+//         }else {
+//             header('location:index.php?page=dashboard');
+//         }
     
-    }
+//     }
 
     
 
 
-}
+// }
 
 

24a9c87064f37f469fb16cb5ade74984f2b46f08
Mohammed Elghanam <144786058+MohammedElghanam@users.noreply.github.com>
2024-01-14 00:43:03 +0100

    Read tags

--- controllers/dashboard_controller.php
+++ controllers/dashboard_controller.php
@@ -42,7 +42,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 
     $sql1 = "SELECT * FROM categories order by create_at desc, edit_at LIMIT 5";
-    $result1 = $objet_dashboard->SELECT($sql1);    
+    $result1 = $objet_dashboard->SELECT($sql1); 
+    $sql2 = "SELECT * FROM tags LIMIT 5";  
+    $result2 = $objet_dashboard->SELECT($sql2); 
     // dd($result1[1]);
 
 

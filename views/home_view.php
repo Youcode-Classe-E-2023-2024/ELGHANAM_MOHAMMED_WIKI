487bbd60dad5430b84ba8781f325b89a1f90d11d
Mohammed Elghanam <144786058+MohammedElghanam@users.noreply.github.com>
2024-01-14 04:47:37 +0100

    add img

--- views/home_view.php
+++ views/home_view.php
@@ -4,9 +4,7 @@
                 <div class="flex items-center justify-between h-8 px-6 mx-auto">
                     <!-- Logo -->
                     
-                        <a href="index.php?page=dashboard" class="py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
-                           logo
-                        </a>
+                        <img src="IMAGE/logo_wiki.png" width="100" class=" z-30" alt="">
                         
                     
                     <!-- Search Input -->
